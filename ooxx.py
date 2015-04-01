4461e8068da6c4fff8c34eca2cd8cf48ae82a188
haipz <haipzm@gmail.com>
2015-04-01 11:08:08 +0800

    Create ooxx.py

---
+++ ooxx.py
@@ -0,0 +1,91 @@
+'''
+
+    author  : haipz
+    site    : haipz.com
+    email   : i@haipz.com
+
+'''
+
+import urllib2, os, re, thread, time
+
+def getHtml(url) :
+    page = urllib2.urlopen(url)
+    html = page.read()
+    return html
+
+def filterComment(source) :
+    pattern = ur'begin comments([\s\S]*?)end comments'
+    matchs = re.search(pattern, source)
+    return matchs.group()
+
+def filterThumbnail(source) :
+    pattern = ur'<img src="([\s\S]*?)\.gif"'
+    reobj = re.compile(pattern)
+    result, number = reobj.subn('', source)
+    return result
+
+def downloadPicture(picurl, picpath, picname) :
+    pic = urllib2.urlopen(picurl)
+    f = open(picpath + picname, 'wb')
+    f.write(pic.read()) 
+    f.close()
+    print 'System: ' + picname + ' saved\n'
+
+choice = int(input("0. ooxx 1. pic : "))
+pagestart = int(input("page start: "))
+pageend = int(input("page end: "))
+
+if choice == 0 :
+    dirname = "ooxx"
+else :
+    dirname = "pic"
+
+path = os.getcwd() + "/" + dirname
+isExists = os.path.exists(path)
+if not isExists :
+    print 'System: ' + path + " created"
+    os.makedirs(path)
+else :
+    print 'System: ' + path + " exists"
+
+initurl = "http://jandan.net/" + dirname + "/"
+
+for pagenum in range(pagestart, pageend) :
+    cururl = initurl + "page-" + str(pagenum)
+    print 'Current url: ' + cururl
+    inithtml = getHtml(cururl)
+    curhtml = filterComment(inithtml)
+
+    pattern = ur'<li id="comment-([\s\S]*?)</li>'
+    reobj = re.compile(pattern)
+    matchs = reobj.findall(curhtml)
+    count0 = 0
+    for match in matchs :
+        count0 = count0 + 1
+        match = filterThumbnail(match)
+
+        oopattern = ur'(?:<span id="cos_support-)(?:\d*?)(?:">)(\d*?)(?:</span>)'
+        xxpattern = ur'(?:<span id="cos_unsupport-)(?:\d*?)(?:">)(\d*?)(?:</span>)'
+        oo = re.search(oopattern, match).group(1)
+        xx = re.search(xxpattern, match).group(1)
+
+        picpattern = ur'(?:src=")([\s\S]*?)(.jpg|.png|.gif)'
+        picobj = re.compile(picpattern)
+        result = picobj.findall(match)
+
+        count1 = 0
+        for pic in result :
+            count1 = count1 + 1
+            picurl = pic[0] + pic[1]
+            picpath = path + '/'
+            picname = str(pagenum) + '_oo' + oo + '_xx' + xx + '_' + str(count0) + '_' + str(count1) + pic[1]
+
+            print 'Infomation:'
+            print 'Picture url: ' + picurl
+            print 'Picture path: ' + picpath
+            print 'Picture name: ' + picname
+
+            try :
+                downloadPicture(picurl, picpath, picname)
+            except Exception as e :
+                print(e)
